cd62e9c95f98e656d69ccf6a7396f693956100d4
Bas M. Dam <automatedchaos@gmail.com>
2017-02-16 18:50:17 +0100

    Added semicolon and wildcard support for all call types (MsBuild, CommandLine, PowerShell, UI-api).
    Folders and no match will deliver no result file.
    Multiple matches on the same file will distinct into a single result.

--- src/Pickles/Pickles/CommandLineArgumentParser.cs
+++ src/Pickles/Pickles/CommandLineArgumentParser.cs
@@ -26,6 +26,7 @@ using System.Reflection;
 using NDesk.Options;
 
 using TextWriter = System.IO.TextWriter;
+using PicklesDoc.Pickles.Extensions;
 
 namespace PicklesDoc.Pickles
 {
@@ -117,9 +118,8 @@ namespace PicklesDoc.Pickles
 
             if (!string.IsNullOrEmpty(this.testResultsFile))
             {
-                var files = this.testResultsFile.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-
-                configuration.AddTestResultFiles(files.Select(f => this.fileSystem.FileInfo.FromFileName(f)));
+                configuration.AddTestResultFiles(
+                    PathExtensions.GetAllFilesFromPathAndFileNameWithOptionalSemicolonsAndWildCards(this.testResultsFile, this.fileSystem));
             }
 
             if (!string.IsNullOrEmpty(this.systemUnderTestName))
