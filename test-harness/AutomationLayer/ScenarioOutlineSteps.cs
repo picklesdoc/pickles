8f03e6815da37ba28a331d6f2ed8eb2cc3840bb2
Dirk Rombauts <dirk.rombauts@questmaster.net>
2017-04-06 17:24:05 +0200

    Fix indentation

--- test-harness/AutomationLayer/ScenarioOutlineSteps.cs
+++ test-harness/AutomationLayer/ScenarioOutlineSteps.cs
@@ -6,48 +6,48 @@ using TechTalk.SpecFlow;
 
 namespace AutomationLayer
 {
-  [Binding]
-  public class ScenarioOutlineSteps
-  {
-    [Then(@"the scenario will '(.*)'")]
-    public void ThenTheScenarioWill(string result)
+    [Binding]
+    public class ScenarioOutlineSteps
     {
-        if (result.ToUpperInvariant().StartsWith("PASS"))
+        [Then(@"the scenario will '(.*)'")]
+        public void ThenTheScenarioWill(string result)
         {
-        // nothing to be done
+            if (result.ToUpperInvariant().StartsWith("PASS"))
+            {
+                // nothing to be done
+            }
+            else if (result.ToUpperInvariant().StartsWith("FAIL"))
+            {
+                Check.That(true).IsEqualTo(false);
+            }
+            else
+            {
+                ScenarioContext.Current.Pending();
+            }
         }
-        else if (result.ToUpperInvariant().StartsWith("FAIL"))
+
+        [When(@"I have special characters for regexes in the value, for example a '(.*)'")]
+        public void WhenIHaveSpecialCharactersForRegexesInTheValueForExampleARegex(string regex)
         {
-        Check.That(true).IsEqualTo(false);
+            // nothing to be done
         }
-        else
+
+        [When(@"I have backslashes in the value, for example a '(.*)'")]
+        public void WhenIHaveBackslashesInTheValueForExampleAFilePath(string filePath)
         {
-        ScenarioContext.Current.Pending();
+            // nothing to be done
+        }
+
+        [When(@"I have parenthesis in the value, for example an '(.*)'")]
+        public void WhenIHaveParenthesisInTheValueForExampleAnOverlyDescriptiveField(string description)
+        {
+            // nothing to be done - test case requires pass
+        }
+
+        [When(@"I have a field with value '(.*)'")]
+        public void WhenIHaveAFieldWithValue(string value)
+        {
+            // nothing to be done - test case requires pass
         }
     }
-
-    [When(@"I have special characters for regexes in the value, for example a '(.*)'")]
-    public void WhenIHaveSpecialCharactersForRegexesInTheValueForExampleARegex(string regex)
-    {
-        // nothing to be done
-    }
-
-    [When(@"I have backslashes in the value, for example a '(.*)'")]
-    public void WhenIHaveBackslashesInTheValueForExampleAFilePath(string filePath)
-    {
-        // nothing to be done
-    }
-
-    [When(@"I have parenthesis in the value, for example an '(.*)'")]
-    public void WhenIHaveParenthesisInTheValueForExampleAnOverlyDescriptiveField(string description)
-    {
-        // nothing to be done - test case requires pass
-    }
-
-    [When(@"I have a field with value '(.*)'")]
-    public void WhenIHaveAFieldWithValue(string value)
-    {
-        // nothing to be done - test case requires pass
-    }
-  }
 }
